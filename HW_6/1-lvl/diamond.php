825d70760135f989b3af71d65649ae95a303c71c
nadin nadin <45623566+nadinnadin@users.noreply.github.com>
2019-10-29 17:33:01 +0200

    Revert "ya sdelal'"

--- HW_6/1-lvl/diamond.php
+++ HW_6/1-lvl/diamond.php
@@ -1,27 +1,27 @@
 <?php
 $handle = fopen("php://stdin","r");
-$diamond = fgets($handle);
+$n = fgets($handle);
 
 if($n % 2 == 0){
 	echo "четное число";
 }
 else{
-	for($i = 1;$i <= $diamond; $i+=2){
-		for($j = 0;$j <= ($diamond-$i)/2; $j++){
+	for($i = 1;$i <= $n; $i+=2){
+		for($j = 0;$j <= ($n-$i)/2; $j++){
 			echo ' ';
 		}
 	echo str_repeat("*", $i);
-		for($j = 0;$j <= ($diamond-$i)/2; $j++){
+		for($j = 0;$j <= ($n-$i)/2; $j++){
 			echo ' ';
 		}
 	echo "\n";
 	}
-	for($i = $diamond - 2;$i >=1; $i-=2){
-		for($j = 0;$j <= ($diamond-$i)/2; $j++){
+	for($i = $n - 2;$i >=1; $i-=2){
+		for($j = 0;$j <= ($n-$i)/2; $j++){
 			echo ' ';
 		}
 	echo str_repeat("*", $i);
-		for($j = 0;$j <= ($diamond-$i)/2; $j++){
+		for($j = 0;$j <= ($n-$i)/2; $j++){
 			echo ' ';
 		}
 	echo "\n";
